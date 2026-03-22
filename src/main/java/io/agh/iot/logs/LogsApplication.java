998958f3d19b88b300c4d17a084a5d907a771cbe
dominiks01 <dominikszot950@gmail.com>
2026-03-22 21:28:30 +0100

    IOTAG: initial commit

---
+++ src/main/java/io/agh/iot/logs/LogsApplication.java
@@ -0,0 +1,11 @@
+package io.agh.iot.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogsApplication.class, args);
+    }
+}
